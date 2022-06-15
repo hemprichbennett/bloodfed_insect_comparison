26da9bf2982ecef77b94d7203638472cb9dd3574
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2022-06-15 16:23:30 +0100

    Added basic annotations

--- scripts/example_code.R
+++ scripts/example_code.R
@@ -1,7 +1,16 @@
+# very basic example code of how I might combine field data with dummy 
+# pacbio and miseq data, plus separate taxonomic classifications that
+# had happened for each (e.g. if they had been sequenced using different
+# marker genes. No real data were harmed in the making of this code.)
+
 library(readr)
 library(tidyr)
 library(magrittr)
 library(dplyr)
+
+
+# read in the data --------------------------------------------------------
+
 field_data <- read_csv('data/raw_data/example_field_data.csv')
 
 miseq_classifications <- read_csv('data/raw_data/example_miseq_classifications.csv') %>%
@@ -15,6 +24,12 @@ pacbio_classifications <- read_csv('data/raw_data/example_pacbio_classifications
   # deduplicate rows
   distinct(.)
 pacbio_metadata <- read_csv('data/raw_data/example_pacbio_data.csv')
+
+
+
+# combine and mung the data -----------------------------------------------
+
+
 
 pacbio_data <- field_data %>%
   left_join(pacbio_metadata) %>%
