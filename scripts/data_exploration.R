9749208570c72f215d44f283f103192e282d2e18
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2022-06-15 15:05:26 +0100

    Compared data to data output from BOLD

--- scripts/data_exploration.R
+++ scripts/data_exploration.R
@@ -1,4 +1,7 @@
-library(tidyverse)
+library(magrittr)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
 library(rentrez)
 
 in_df <- readxl::read_excel('data/raw_data/29_9_21 Heath trap SMRT-00672_mBraveoutput_EVZ20210929-1.xlsm')
@@ -30,3 +33,28 @@ more_info %>%
   filter(Phylum == 'Arthropoda') %>%
   group_by(Order) %>%
   summarise(nrows = n())
+
+
+
+bold_data <- readxl::read_excel('~/Dropbox/work/projects/in_progress/bin_accumulation/data/processed_data/TMGHA/bold_tidied.xlsx')
+
+
+big_df <- in_df %>%
+  left_join(bold_data, by = c('ProcessID' = 'Process ID'))
+
+
+chordate_df <- big_df %>%
+  filter(Phylum == 'Chordata')
+
+
+duplicate_processids <- big_df %>%
+  group_by(ProcessID) %>%
+  summarise(nrows = n()) %>%
+  filter(nrows >1) %>%
+  pull(ProcessID)
+
+just_duplicates <- big_df %>%
+  filter(ProcessID  %in% duplicate_processids)
+
+
+temp <- filter(just_duplicates, ProcessID == 'TMGHA1001-21')
