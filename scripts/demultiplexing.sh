ee11b265ce30f73bb58a2fd7a4958c6bf93156cc
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2022-12-21 11:29:30 +0000

    Modified base code for our purposes, saved primers

--- scripts/demultiplexing.sh
+++ scripts/demultiplexing.sh
@@ -2,7 +2,10 @@
 # should be ran in projects base directory
 cutadapt \
     -e 0.15 --no-indels \
-    -g ^file:barcodes_fwd.fasta \
-    -G ^file:barcodes_rev.fasta \
-    -o {name1}-{name2}.1.fastq.gz -p {name1}-{name2}.2.fastq.gz \
-    input.1.fastq.gz input.2.fastq.gz
+    -g ^file:data/primers/barcodes_fwd.fasta \
+    -G ^file:data/primers/barcodes_rev.fasta \
+    -o ./data/processed_data/demultiplexed/{name1}-{name2}.1.fastq.gz \
+    -p ./data/processed_data/demultiplexed/{name1}-{name2}.2.fastq.gz \
+    BB_TMBM/BM01/Blood-Meal-01-CCDB-35500-511-3547548/FASTQ_Generation_2022-10-22_00_19_30Z-4477526/101922Blood-Meal-1_L001-ds.24b9d774095b4073968cae4add9331f2/101922Blood-Meal-1_S1_L001_R1_001.fastq.gz \
+    BB_TMBM/BM01/Blood-Meal-01-CCDB-35500-511-3547548/FASTQ_Generation_2022-10-22_00_19_30Z-4477526/101922Blood-Meal-1_L001-ds.24b9d774095b4073968cae4add9331f2/101922Blood-Meal-1_S1_L001_R2_001.fastq.gz
+
