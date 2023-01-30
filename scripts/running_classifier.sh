1ff1d01c4a1c4bcc76d4e4d4b4f38450c29d4504
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2023-01-30 11:09:44 +0000

    Fixed extensions, ran classifier

--- scripts/running_classifier.sh
+++ scripts/running_classifier.sh
@@ -1,10 +1,22 @@
 
 qiime tools import \
   --input-path  data/processed_data/interactions/BMO1/all_asvs.fasta \
-  --output-path data/processed_data/BM01.fasta \
+  --output-path data/processed_data/BM01_asvs.qza \
   --type 'FeatureData[Sequence]'
+
+qiime feature-classifier classify-sklearn \
+     --i-classifier data/reference_library/processed_data/2023-01/16s/ncbi-refseqs-classifier.qza \
+     --i-reads data/processed_data/BM01_asvs.qza \
+     --o-classification data/processed_data/BM01_taxonomy.qza
 
 qiime tools import \
   --input-path  data/processed_data/interactions/BMO2/all_asvs.fasta \
   --output-path data/processed_data/BM02.fasta \
   --type 'FeatureData[Sequence]'
+
+qiime feature-classifier classify-sklearn \
+     --i-classifier data/reference_library/processed_data/2023-01/16s/ncbi-refseqs-classifier.qza \
+     --i-reads data/processed_data/BM02_asvs.qza \
+     --o-classification data/processed_data/BM02_taxonomy.qza
+
+     
